fb51d256e98b38096cb7d6e23f3b84a8ecceee6f
Shashaank Singh <tapash2004@outlook.com>
2021-06-02 00:09:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-import discord
-from discord.ext.commands
+import os,discord
+from discord.ext import commands
+
+client=commands.Bot(command_prefix="!",activity=discord.Activity(type=discord.ActivityType.watching, name="your messages!"))
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+    print(f'Logged in as {client.user.name} - {client.user.id}')
+
+@client.event
+async def on_interaction(itr):
+    await itr.send_message(f"Received an Interaction.```py\n{itr}\n```")
+
+client.run(os.environ['BOT_TOKEN'])
