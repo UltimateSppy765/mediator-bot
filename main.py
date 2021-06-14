a3965ae7baaeb255a163758c3f92a025a639b8f1
Shashaank Singh <tapash2004@outlook.com>
2021-06-14 05:36:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os,discord
 from discord.ext import commands
 
+cogs=["imports.cogs.wipe"] 
 client=commands.Bot(command_prefix="!",activity=discord.Activity(type=discord.ActivityType.watching, name="your messages!",allowed_mentions=discord.AllowedMentions(roles=False)))
 
 @client.event
@@ -13,4 +14,10 @@ async def on_interaction(itr):
     if itr.type==discord.InteractionType.application_command and itr.data["name"]=="ping":
         await itr.response.send_message(f"Bot ping is `{round(client.latency*1000)}ms`.",ephemeral=True)
 
+for i in cogs:
+    try:
+        client.load_extension(i)
+    except:
+        client.get_channel(853786766883291146)
+        c.send(f"Cog `{i}` failed to load.")
 client.run(os.environ['BOT_TOKEN'])
