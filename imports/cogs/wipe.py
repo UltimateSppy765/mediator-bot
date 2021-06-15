ab8770b43e79dd226bb435fb5616ff47a80b5288
UltimateSppy765 <tapash2004@outlook.com>
2021-06-15 21:32:09 +0530

    Update wipe.py

--- imports/cogs/wipe.py
+++ imports/cogs/wipe.py
@@ -7,7 +7,7 @@ class wipe(commands.Cog):
         self.client = client
 
     @commands.has_permissions(manage_messages=True)
-    @commands.group()
+    @commands.group(invoke_without_command=True)
     async def wipe(self,ctx):
         return await ctx.reply("<:mno:851569517242351616> Missing or Invalid Subcommand.") 
     @wipe.command()
@@ -15,7 +15,7 @@ class wipe(commands.Cog):
         if count>150 or count<1:
             return await ctx.reply("<:merror:851584410935099423> Please enter a count between 1 and 150.")
         msg=await ctx.reply("<:mwiping:851682672593731596> Wiping Messages...",mention_author=False)
-        pur=await ctx.channel.purge(limit=count,before=ctx.id)
+        pur=await ctx.channel.purge(limit=count,before=ctx.message.id)
         await msg.edit(f"<:mwipeyay:851572058382925866> Successfully wiped {len(pur)} messages.")
         return
 
