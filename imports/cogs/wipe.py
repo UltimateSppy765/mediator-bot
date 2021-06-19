b2cf0a3304d106de624f7e4167abd71a225a7f78
UltimateSppy765 <tapash2004@outlook.com>
2021-06-19 16:01:24 +0530

    Update wipe.py

--- imports/cogs/wipe.py
+++ imports/cogs/wipe.py
@@ -9,12 +9,12 @@ class wipe(commands.Cog):
     async def on_interaction(self,itr):
         if itr.type==2 and itr.data["name"]=="wipe":
             if itr.data["options"][0]["name"]=="off":
-	        try:
-	            guild=itr.guild					
-	        except:
-	            return await itr.response.send_message("<:merror:851584410935099423> This command cannot be used in Direct Messages.",ephemeral=True)
+                try:
+                    guild=itr.guild
+                except:
+                    return await itr.response.send_message("<:merror:851584410935099423> This command cannot be used in Direct Messages.",ephemeral=True)
 	        if itr.channel.permissions_for(itr.user).manage_messages:
-		    pass
+                    pass
 	        else:
 	            return await itr.response.send_message("<:merror:851584410935099423> You cannot use this command.",ephemeral=True)
                 try:
