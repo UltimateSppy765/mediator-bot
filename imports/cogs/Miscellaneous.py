de8d39989a65fa52e49ff921b10ccf84ef03b527
UltimateSppy765 <tapash2004@outlook.com>
2021-06-20 18:06:44 +0530

    Update Miscellaneous.py

--- imports/cogs/Miscellaneous.py
+++ imports/cogs/Miscellaneous.py
@@ -9,15 +9,12 @@ class Miscellaneous(commands.Cog):
     async def on_interaction(self,itr):
         if itr.type==2:
             if itr.data["name"]=="ping":
-                await self.pingslash(itr)
+                await itr.response.send_message(f"Bot ping is `{round(self.client.latency*1000)}ms`.",ephemeral=True)
     
     @commands.command()
     async def ping(self,ctx):
         "Shows bot latency."
         await ctx.reply(f"Bot ping is `{round(self.client.latency*1000)}ms`.",mention_author=False)
-    
-    async def pingslash(self,itr):
-        await itr.response.send_message(f"Bot ping is `{round(self.client.latency*1000)}ms`.",ephemeral=True)
 
 def setup(client):
     client.add_cog(Miscellaneous(client))
