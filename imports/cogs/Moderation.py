5d9d6c018de39551ed8827dcec84c7605a89974b
UltimateSppy765 <tapash2004@outlook.com>
2021-06-20 18:33:56 +0530

    Update Moderation.py

--- imports/cogs/Moderation.py
+++ imports/cogs/Moderation.py
@@ -45,11 +45,12 @@ class Moderation(commands.Cog):
             return await itr.response.send_message("<:merror:851584410935099423> This command cannot be used in Direct Messages.",ephemeral=True)
         if perms==False:
             return await itr.response.send_message("<:merror:851584410935099423> You cannot use this command.",ephemeral=True)
-        if itr.channel.permissions_for(self.user).manage_messages==False or itr.channel.permissions_for(self.user).read_message_history==False:
-            if itr.channel.permissions_for(self.user).manage_messages==False and itr.channel.permissions_for(self.user).read_message_history==False:
+        mem=itr.guild.get_member(itr.application_id)
+        if itr.channel.permissions_for(mem).manage_messages==False or itr.channel.permissions_for(mem).read_message_history==False:
+            if itr.channel.permissions_for(self.user).manage_messages==False and itr.channel.permissions_for(mem).read_message_history==False:
                 missperms="Read Message History\nManage Messages"
             else:
-                missperms="Read Message History" if itr.channel.permissions_for(self.user).read_message_history==False else "Manage Messages"
+                missperms="Read Message History" if itr.channel.permissions_for(mem).read_message_history==False else "Manage Messages"
             return await itr.response.send_message(f"<:merror:851584410935099423> I do not have the required permissions to perform this, please check my permissions for this channel:```\n{missperms}\n```",ephemeral=True)
         try:
             count=itr.data["options"][0]["options"][0]["value"]
