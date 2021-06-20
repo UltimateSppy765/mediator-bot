b5b943a0587564b5f3d41c90453aabeda4ea94eb
UltimateSppy765 <tapash2004@outlook.com>
2021-06-20 17:33:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os,discord,traceback
 from discord.ext import commands
 
-coggs=["imports.cogs.Moderation"] 
+coggs=["imports.cogs.Moderation","imports.cogs.Miscellaneous"] 
 client=commands.Bot(command_prefix="!",activity=discord.Activity(type=discord.ActivityType.watching, name="your messages!",allowed_mentions=discord.AllowedMentions(roles=False)))
 
 @client.event
@@ -9,15 +9,11 @@ async def on_ready():
     print('Bot is ready.')
     print(f'Logged in as {client.user.name} - {client.user.id}')
 
-@client.event
-async def on_interaction(itr):
-    if itr.type==discord.InteractionType.application_command and itr.data["name"]=="ping":
-        await itr.response.send_message(f"Bot ping is `{round(client.latency*1000)}ms`.",ephemeral=True)
-
 for i in coggs:
     try:
         client.load_extension(i)
     except:
         print(f"Failed to load cog: {i}")
         print(traceback.format_exc())
+
 client.run(os.environ['BOT_TOKEN'])
