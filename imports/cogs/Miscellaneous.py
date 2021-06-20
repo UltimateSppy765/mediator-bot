2c4da199892eadb91e154319c15fb654bb86e0c4
UltimateSppy765 <tapash2004@outlook.com>
2021-06-20 17:31:35 +0530

    Update and rename ping.py to Miscellaneous.py

---
+++ imports/cogs/Miscellaneous.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class Miscellaneous(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.Cog.listener()
+    async def on_interaction(self,itr):
+        if itr.type==2:
+            if itr.data["name"]=="ping":
+                return await self.pingslash(itr)
+    
+    @commands.command()
+    async def ping(self,ctx):
+        "Shows bot latency."
+        await ctx.send(f"Bot ping is `{round(self.client.latency*1000)}ms`.")
+    
+    async def pingslash(self,itr):
+        await itr.response.send_message(f"Bot ping is `{round(self.client.latency*1000)}ms`.",ephemeral=True)
+
+def setup(client):
+    client.add_cog(Miscellaneous(client))
