ac33bb6bdb3a05390eb979acdf3250456205f6a8
UltimateSppy765 <tapash2004@outlook.com>
2021-06-20 17:58:30 +0530

    Update Moderation.py

--- imports/cogs/Moderation.py
+++ imports/cogs/Moderation.py
@@ -49,7 +49,7 @@ class Moderation(commands.Cog):
             if itr.channel.permissions_for(self.user).manage_messages==False and itr.channel.permissions_for(self.user).read_message_history==False:
                 missperms="Read Message History\nManage Messages"
             else:
-                missperms="Read Message History" if itr.channel.permissions_for(self.user).read_message_history=False else "Manage Messages"
+                missperms="Read Message History" if itr.channel.permissions_for(self.user).read_message_history==False else "Manage Messages"
             return await itr.response.send_message(f"<:merror:851584410935099423> I do not have the required permissions to perform this, please check my permissions for this channel:```\n{missperms}\n```",ephemeral=True)
         try:
             count=itr.data["options"][0]["options"][0]["value"]
