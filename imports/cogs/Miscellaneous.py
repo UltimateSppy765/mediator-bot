42c2357622ae391ba1fc2b500ebc7bb3584b2996
UltimateSppy765 <tapash2004@outlook.com>
2021-10-07 12:21:58 +0530

    Update Miscellaneous.py

--- imports/cogs/Miscellaneous.py
+++ imports/cogs/Miscellaneous.py
@@ -6,5 +6,10 @@ class Miscellaneous(commands.Cog):
     def __init__(self,client):
         self.client=client
 
+    @commands.slash_command()
+    async def ping(self,itr):
+        """Shows bot ping/latency."""
+        await itr.response.send_message(f'Bot latency is `{round(self.client.latency*1000)}ms`.',ephemeral=True)
+
 def setup(client):
     client.add_cog(Miscellaneous(client))
