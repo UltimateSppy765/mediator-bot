b1e854598cc313f15eb11c5c9ae4cae47443db87
UltimateSppy765 <tapash2004@outlook.com>
2021-10-07 11:14:41 +0530

    Update Miscellaneous.py

--- imports/cogs/Miscellaneous.py
+++ imports/cogs/Miscellaneous.py
@@ -1,8 +1,10 @@
 import disnake as discord
 from disnake.ext import commands
+from imports.modules.perspective import getscore
 
 class Miscellaneous(commands.Cog):
     def __init__(self,client):
         self.client=client
 
-def setup
+def setup(client):
+    client.add_cog()
