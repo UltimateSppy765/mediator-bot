b8d1503d1ad4f9aee98df1f2fa4ab1478fbb53fc
UltimateSppy765 <tapash2004@outlook.com>
2021-10-08 14:49:24 +0530

    Cog command changes

--- mediator/main.py
+++ mediator/main.py
@@ -42,15 +42,18 @@ async def loadedcogs(itr):
     if cglist==[]:
         return await itr.response.send_message(':gear: No cogs are loaded at the moment.',ephemeral=True)
     for i in cglist:
-        cogstr=cogstr+f"{i} - {inspect.getfile(maps[i].__class__)}\n"
+        cogstr=cogstr+f"• {i} - {inspect.getfile(maps[i].__class__)}\n"
     e1=discord.Embed(description=f"```\n{cogstr}```",color=discord.Color(3092791))
     await itr.response.send_message(content=":scroll: Here are the cogs currently loaded:",embed=e1,ephemeral=True)
 
 @cogutils.sub_command(name="list-cogs")
 async def listcogs(itr):
+    lst=cogfunc.getcogs()
+    if lst==[]:
+    return await itr.response.send_message(':gear: No cogs are specified in the config file.',ephemeral=True)
     cogstr=""
-    for i in cogfunc.getcogs():
-        cogstr=cogstr+f"{i['name']} ({i['path']}) - {i['description']}\n"
+    for i in lst:
+        cogstr=cogstr+f"• {i['name']} ({i['path']}) - {i['description']}\n"
     e1=discord.Embed(description=f"```\n{cogstr}```",color=discord.Color(3092791))
     await itr.response.send_message(content=":scroll: Here are the cogs listed in the config file:",embed=e1,ephemeral=True)
 
