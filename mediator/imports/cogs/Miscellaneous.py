1fc11bf9d46ee6e5a77fe24416b4af233caa18f8
UltimateSppy765 <tapash2004@outlook.com>
2021-10-14 03:01:09 +0530

    Update Miscellaneous.py

--- mediator/imports/cogs/Miscellaneous.py
+++ mediator/imports/cogs/Miscellaneous.py
@@ -12,5 +12,18 @@ class Miscellaneous(commands.Cog):
         """Shows bot ping/latency."""
         await itr.response.send_message(f'Bot latency is `{round(self.client.latency*1000)}ms`.',ephemeral=True)
 
+    @command.slash_command()
+    async def toxicitycheck(self,itr,text:str,hidden:bool=True):
+        await itr.response.defer(ephemeral=hidden)
+        text=text.strip()
+        score=getscore(text)
+        if score>=80:
+            text=f"||{text}||"
+        desc=f"**Text you input:** {text}\n__Toxicity Score:__```\n{score}%\n```"
+        if score==50:
+            desc=desc+"**__Note:__** The API might've failed to determine a value for the given text, so it's toxicity percentage is set to `50%`" 
+        embed1=discord.Embed(title=":test_tube: Toxicity Check",description=desc,colour=3092791)
+        return await itr.edit_original_message(embed=embed1)
+
 def setup(client):
     client.add_cog(Miscellaneous(client))
