099b5a90fbcceb5adf9dd9c71cd7310e761d857e
UltimateSppy765 <tapash2004@outlook.com>
2021-10-19 14:12:35 +0530

    Update Moderation.py

--- mediator/imports/cogs/Moderation.py
+++ mediator/imports/cogs/Moderation.py
@@ -9,8 +9,16 @@ class Moderation(commands.Cog):
     @commands.bot_has_permissions(read_message_history=True,manage_messages=True)
     @commands.slash_command()
     async def wipe(self,itr):
-        print(itr.filled_options)
-        pass
+        keys=itr.options.keys()
+        try:
+            count=itr.options[keys[0]]['count']
+        except KeyError:
+            pass
+        else:
+            if count not in range(1,201):
+                return await itr.response.send_message(':x: The message count cannot be less than 1 or more than 200.',ephemeral=True)
+            else:
+                pass
 
     @wipe.sub_command()
     async def off(self,itr,count:int=20,ephemeral:bool=False):
