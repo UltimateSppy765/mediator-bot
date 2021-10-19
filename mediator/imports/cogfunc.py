c83d990cf308bd0e62fb4349931332cf62595fc3
UltimateSppy765 <tapash2004@outlook.com>
2021-10-19 12:27:31 +0530

    Update cogfunc.py

--- mediator/imports/cogfunc.py
+++ mediator/imports/cogfunc.py
@@ -1,6 +1,6 @@
 import json
 
 def getcogs():
-    file=open('mediator/coglist.json')
-    data=json.load(file)
+    with open('mediator/coglist.json','r') as file:
+        data=json.load(file)
     return data['cog_list']
