cbfa4411e7760c712b7c53c4baa1dff4a49a5882
UltimateSppy765 <tapash2004@outlook.com>
2022-02-17 08:24:01 +0530

    Update main.py

--- mediator/main.py
+++ mediator/main.py
@@ -1,1 +1,7 @@
-#Hello World
+import os
+import disnake as discord
+from disnake.ext import commands
+
+client=commands.InteractionBot(sync_commands=False,activity=discord.Activity(type=discord.ActivityType.watching,name='your messages!'),allowed_mentions=discord.AllowedMentions(everyone=False,roles=False))
+
+client.run(os.environ['BOT_TOKEN']
