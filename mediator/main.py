0ef27f781e85157b93cc7be08ddce039eb58c312
UltimateSppy765 <tapash2004@outlook.com>
2022-03-28 03:33:55 +0530

    Add return type declarations to more functions

--- mediator/main.py
+++ mediator/main.py
@@ -8,12 +8,12 @@ class SomeClient(commands.Bot):
     def __init__(self) -> None:
         super().__init__(command_prefix='!', activity=activity, intents=intents)
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print(f'Bot is ready.\nLogged in as {str(self.user)} - {self.user}')
 
 client = SomeClient()
 
-async def main():
+async def main() -> None:
     async with client:
         await client.start(os.environ['BOT_TOKEN'])
 
