182ca54e5d5a9b5186582a0271a032991492a5fb
UltimateSppy765 <tapash2004@outlook.com>
2022-03-28 16:15:46 +0530

    I stg lints are triggering me

--- mediator/imports/cogs/Miscellaneous.py
+++ mediator/imports/cogs/Miscellaneous.py
@@ -8,12 +8,10 @@ from discord.ext import commands
 class Miscellaneous(commands.Cog):
     """Contains miscellaneous commands."""
 
-
     def __init__(self, client) -> None:
         self.client = client
         with open(client.l10nlist["Miscellaneous"], "r") as file:
             self.l10ndata = json.load(file)
-
 
     @app_commands.command(name="ping")
     async def ping(self, itr: discord.Interaction) -> None:
@@ -21,7 +19,7 @@ class Miscellaneous(commands.Cog):
             self.l10ndata["ping"]["response"][str(itr.locale)].format(
                 round(self.client.latency * 1000)
             ),
-            ephemeral=True
+            ephemeral=True,
         )
 
 
