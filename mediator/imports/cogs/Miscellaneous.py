4eebdf858e2a8743eca9ebc006064a38f74c217d
UltimateSppy765 <tapash2004@outlook.com>
2022-03-28 16:02:40 +0530

    Update Miscellaneous.py

--- mediator/imports/cogs/Miscellaneous.py
+++ mediator/imports/cogs/Miscellaneous.py
@@ -10,13 +10,12 @@ class Miscellaneous(commands.Cog):
     
     def __init__(self, client) -> None:
         self.client = client
-        with open("mediator/coglist.json", "r") as file:
-            with open(json.load(file)["l10npath"]["Miscellaneous"]) as file2:
-                self.l10ndata = json.load(file2)
+        with open(client.l10nlist["Miscellaneous"], "r") as file:
+            self.l10ndata = json.load(file)
         
     @app_commands.command(name="ping")
     async def ping(self, itr: discord.Interaction) -> None:
-        await itr.response.send_message(f":ping_pong: Bot latency is `{round(self.client.latency * 1000)}ms`", ephemeral=True)
+        await itr.response.send_message(self.l10ndata["ping"]["response"][str(itr.locale)].format(round(self.client.latency * 1000)), ephemeral=True)
     
 
 async def setup(client) -> None:
